7a0c4afac980df57145035cb97ea2e76d16b020b
Anfimov Dima <danfimov@yandex-team.ru>
2024-09-18 17:54:31 +0200

    fix: make token not requred (for tests)

--- bread_fast_bot/settings.py
+++ bread_fast_bot/settings.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    token: str
+    token: str = ''
 
     model_config = SettingsConfigDict(env_file="conf/.env")
 
